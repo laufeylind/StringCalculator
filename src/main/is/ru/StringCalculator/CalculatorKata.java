8aac726391c08f21cba603234820d6645f1781f4
laufeylind <laufeys14@ru.is>
2015-10-16 00:24:13 +0000

    Refactoring - Delimiter

--- src/main/is/ru/StringCalculator/CalculatorKata.java
+++ src/main/is/ru/StringCalculator/CalculatorKata.java
@@ -13,29 +13,30 @@ public class CalculatorKata{
 		this.numbers = numbers;
 	}
 
-
 	public static int add(String text){
 		if (text.isEmpty()){
 			return 0;
 		}
 
-		CalculatorKata calculator;
-		String delimiter = ",|\n";
-		if (text.startsWith("//")){
-			String parts[] = text.split("\n", 2);
-			delimiter = parts[0].substring(2);
-			text = parts[1];
-			calculator = new CalculatorKata(delimiter, text);
-		}
-		else{
-			calculator = new CalculatorKata(",|\n", text);
-		}
+		CalculatorKata calculator = parseText(text);
 
-		Stream<String> numbers = Arrays.stream(text.split(delimiter));
+		Stream<String> numbers = Arrays.stream(calculator.numbers.split(calculator.delimiter));
 		return numbers.mapToInt(Integer::parseInt).sum();
 
 	}
 
-	
+	private static CalculatorKata parseText(String text){
+		CalculatorKata calculator;
+		
+		if (text.startsWith("//")){
+			String parts[] = text.split("\n", 2);
+			text = parts[1];
+			calculator = new CalculatorKata(parts[0].substring(2), parts[1]);
+		}
+		else {
+			calculator = new CalculatorKata(",|\n", text);
+		}
+		return calculator;
+	}
 	
 }
